d6cddfcedb507124fc885ee148a93b3006570882
Rene Murillo <contact@renemurillo.net>
2019-09-29 15:48:51 +0100

    Services component ready

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,18 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 
 import {Link} from 'react-router-dom';
+import Services from '../components/Services';
 const Home = () => {
     return (
-        <Hero>
-            <Banner title="Luxurious Rooms" subtitle="Deluxe rooms starting at $299">
-                <Link to="/rooms" className="btn-primary">Our rooms</Link>
-            </Banner>
-        </Hero>
+        <React.Fragment>
+            <Hero>
+                <Banner title="Luxurious Rooms" subtitle="Deluxe rooms starting at $299">
+                    <Link to="/rooms" className="btn-primary">Our rooms</Link>
+                </Banner>
+            </Hero>
+            
+            <Services />
+        </React.Fragment>
     );
 }
 
